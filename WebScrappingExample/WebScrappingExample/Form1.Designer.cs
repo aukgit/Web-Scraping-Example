9d8280bac1c028418ef07ffcd0f7f58626852518
Md. Alim Ul Karim <devorg.bd@gmail.com>
2016-10-07 00:51:30 +0600

    Scrapping Sample Done

--- WebScrappingExample/WebScrappingExample/Form1.Designer.cs
+++ WebScrappingExample/WebScrappingExample/Form1.Designer.cs
@@ -23,22 +23,24 @@
         /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent() {
-            this.button1 = new System.Windows.Forms.Button();
+            this.scapBtn = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.UrlTextBox = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.AbortBtn = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // button1
+            // scapBtn
             // 
-            this.button1.Location = new System.Drawing.Point(17, 96);
-            this.button1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(87, 30);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Scrap";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.scapBtn.Location = new System.Drawing.Point(17, 96);
+            this.scapBtn.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.scapBtn.Name = "scapBtn";
+            this.scapBtn.Size = new System.Drawing.Size(87, 30);
+            this.scapBtn.TabIndex = 0;
+            this.scapBtn.Text = "Scrap";
+            this.scapBtn.UseVisualStyleBackColor = true;
+            this.scapBtn.Click += new System.EventHandler(this.button1_Click);
             // 
             // label1
             // 
@@ -49,14 +51,14 @@
             this.label1.TabIndex = 1;
             this.label1.Text = "URL to scrap";
             // 
-            // textBox1
+            // UrlTextBox
             // 
-            this.textBox1.Location = new System.Drawing.Point(17, 63);
-            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(529, 25);
-            this.textBox1.TabIndex = 2;
-            this.textBox1.Text = "http://economictimes.indiatimes.com/markets/stocks/stock-quotes?ticker=a";
+            this.UrlTextBox.Location = new System.Drawing.Point(17, 63);
+            this.UrlTextBox.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.UrlTextBox.Name = "UrlTextBox";
+            this.UrlTextBox.Size = new System.Drawing.Size(529, 25);
+            this.UrlTextBox.TabIndex = 2;
+            this.UrlTextBox.Text = "http://economictimes.indiatimes.com/markets/stocks/stock-quotes?ticker=%d";
             // 
             // label2
             // 
@@ -68,15 +70,37 @@
             this.label2.TabIndex = 3;
             this.label2.Text = "Web Scrapper Sample";
             // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(206, 96);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(340, 29);
+            this.progressBar1.TabIndex = 4;
+            this.progressBar1.Visible = false;
+            // 
+            // AbortBtn
+            // 
+            this.AbortBtn.Location = new System.Drawing.Point(110, 96);
+            this.AbortBtn.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.AbortBtn.Name = "AbortBtn";
+            this.AbortBtn.Size = new System.Drawing.Size(87, 30);
+            this.AbortBtn.TabIndex = 5;
+            this.AbortBtn.Text = "Abort";
+            this.AbortBtn.UseVisualStyleBackColor = true;
+            this.AbortBtn.Visible = false;
+            this.AbortBtn.Click += new System.EventHandler(this.AbortBtn_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(563, 161);
+            this.Controls.Add(this.AbortBtn);
+            this.Controls.Add(this.progressBar1);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.UrlTextBox);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.scapBtn);
             this.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
@@ -91,10 +115,12 @@
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button scapBtn;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox UrlTextBox;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button AbortBtn;
     }
 }
 
--- WebScrappingExample/WebScrappingExample/Form1.cs
+++ WebScrappingExample/WebScrappingExample/Form1.cs
@@ -3,19 +3,105 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using HtmlAgilityPack;
+using HtmlDocument = System.Windows.Forms.HtmlDocument;
 
 namespace WebScrappingExample {
     public partial class Form1 : Form {
+        private Thread thread;
+        private bool threadRunning;
+
+
         public Form1() {
             InitializeComponent();
         }
 
         private void button1_Click(object sender, EventArgs e) {
+            thread = new Thread(() => {
+                try {
+                    threadRunning = true;
+                    this.Invoke(new MethodInvoker(delegate {
+                        scapBtn.Enabled = false;
+                        AbortBtn.Visible = true;
+                        progressBar1.Visible = true;
+                    }));
 
+                    HtmlWeb htmlWeb = new HtmlWeb() {
+                        AutoDetectEncoding = false,
+                        OverrideEncoding = Encoding.GetEncoding("iso-8859-2")
+                    };
+                    var urlTemplate = UrlTextBox.Text;
+                    var notePadList = new List<string>(5000);
+                    for (int i = 'A'; i <= 'Z'; i++) {
+                        var alphabet = ((char) i).ToString();
+                        var parsingUrl = urlTemplate.Replace("%d", alphabet);
+                        var htmlDocument = htmlWeb.Load(parsingUrl);
+                        var firstOrDefault = htmlDocument.
+                            DocumentNode.
+                            Descendants("ul").
+                            FirstOrDefault(n => n.Attributes.Contains("class") &&
+                                                n.Attributes["class"].Value.Equals("companyList"));
+                        notePadList.Add(parsingUrl);
+                        if (firstOrDefault != null) {
+                            var listItems = firstOrDefault
+                                .Descendants("li").ToList();
+
+                            this.Invoke(new MethodInvoker(delegate {
+                                progressBar1.Value = 0;
+                                progressBar1.Maximum = listItems.Count;
+                            }));
+
+                            foreach (var item in listItems) {
+                                var anchorFound = item.Descendants("a").FirstOrDefault();
+                                if (anchorFound != null) {
+                                    var companyName = anchorFound.InnerText;
+                                    notePadList.Add(companyName);
+                                }
+                                this.Invoke(new MethodInvoker(delegate {
+                                    progressBar1.Value++;
+                                }));
+                            }
+                        }
+                    }
+                    File.WriteAllLines("Company-list.txt", notePadList);
+
+
+                    this.Invoke(new MethodInvoker(delegate {
+                        MessageBox.Show(@"File has been written at [Company-list.txt] A-Z all companies saved.", @"Running", MessageBoxButtons.OK,
+                            MessageBoxIcon.Information);
+                        scapBtn.Enabled = true;
+                        AbortBtn.Visible = false;
+                        progressBar1.Visible = false;
+                    }));
+
+                    threadRunning = false;
+                    thread.Abort();
+                    thread = null;
+                } catch (Exception ex) {
+                    // ignored
+                }
+            });
+            thread.Start();
+        }
+
+        private void AbortBtn_Click(object sender, EventArgs e) {
+            if (thread != null) {
+                threadRunning = false;
+                thread.Abort();
+                this.Invoke(new MethodInvoker(delegate {
+                    MessageBox.Show(@"Aborting by the user.", @"Running", MessageBoxButtons.OK,
+                        MessageBoxIcon.Information);
+                    scapBtn.Enabled = true;
+                    AbortBtn.Visible = false;
+                    progressBar1.Visible = false;
+                }));
+            }
         }
     }
 }
